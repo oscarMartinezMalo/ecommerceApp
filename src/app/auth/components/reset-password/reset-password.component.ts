8c79afc59b50428ff177d8bc1e26ccd4c86195b0
Oscar <ommalor@gmail.com>
2021-02-18 14:49:50 -0500

    Add Message  to Reset Password

--- src/app/auth/components/reset-password/reset-password.component.ts
+++ src/app/auth/components/reset-password/reset-password.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from 'shared/components/modal/modal.component';
 import { AppError } from 'shared/errors/app-error';
 import { WrongCredentialError } from 'shared/errors/wrong-crendential-error';
 import { AuthService } from 'shared/services/auth.service';
@@ -15,7 +18,9 @@ export class ResetPasswordComponent implements OnInit {
 
   constructor(    
     private fb: FormBuilder,
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router,
+    private modalService: NgbModal
   ) { 
 
     this.resetPasswordForm = this.fb.group({
@@ -36,9 +41,12 @@ export class ResetPasswordComponent implements OnInit {
     if (this.resetPasswordForm.valid && this.resetPasswordForm.touched) {
       const currentPassword = this.resetPasswordForm.get('currentPassword').value.trim();
       const newPassword = this.resetPasswordForm.get('newPassword').value.trim();
-      console.log(newPassword);
+
       this.authService.resetPassword( {currentPassword, newPassword} )
-        .subscribe(resp => {
+        .subscribe(async resp => {
+          this.authService.logOut();
+          await this.openPopUp();
+          this.router.navigate(['/products']);
         },
         (error: AppError) => {
           if (error instanceof WrongCredentialError) {
@@ -48,6 +56,12 @@ export class ResetPasswordComponent implements OnInit {
            }
         });
     }
+  }
+
+  async openPopUp() {
+    const modalRef = this.modalService.open(ModalComponent);
+    modalRef.componentInstance.title = 'Password successfully changed';
+    modalRef.componentInstance.message = 'Please Login with your new password.';
   }
   
 }
